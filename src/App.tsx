dbf43ffe3a8e41e5193cfeefc1880844026cd33c
Sajid Qureshi <qureshisajid322@gmail.com>
2026-02-04 21:48:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material"
+
+function App() {
+  return (
+    <Box>
+      <Typography variant="h1">Study Tracker</Typography>
+    </Box>
+  )
+}
+
+export default App
